d501c059bc8b5b3ff61a43ec6f60cd13574dead5
Firas Tayeb <firas.tayeb@virtual-identity.com>
2020-03-04 10:43:45 +0100

    Add automated tests

--- src/main/java/com/virtualidentity/firstproject/authors/rest/TestVIController.java
+++ src/main/java/com/virtualidentity/firstproject/authors/rest/TestVIController.java
@@ -1,27 +1,20 @@
 package com.virtualidentity.firstproject.authors.rest;
 
 import com.virtualidentity.firstproject.common.rest.controller.BaseController;
-import com.virtualidentity.firstproject.generated.AuthorsApi;
-import com.virtualidentity.firstproject.generated.TestApi;
+import com.virtualidentity.firstproject.generated.TestVIApi;
 import com.virtualidentity.firstproject.generated.model.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
-public class TestVIController extends BaseController implements TestApi {
+@Controller
+public class TestVIController extends BaseController implements TestVIApi {
 
     @Override
-    public ResponseEntity<Test> getStringTest(
-            @RequestParam(value = "limit", required = false, defaultValue = "0") Integer limit,
-            @RequestParam(value = "offset", required = false, defaultValue = "0") Integer offset) {
+    public ResponseEntity<TestVI> getStringTest() {
 
-        Test vi = new Test().value("Virtual Identity");
+        TestVI vi = new TestVI().value("Virtual Identity");
 
-        return new ResponseEntity<Test>(vi, HttpStatus.OK);
+        return new ResponseEntity<>(vi, HttpStatus.OK);
     }
 }
